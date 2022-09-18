f1a5e036e744958a1342bea20948dae18cb75e1a
Jimmy Clowes <jamesclowes@hotmail.co.uk>
2022-09-18 15:24:39 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Rank based modelling of chocolate preferences',
+    author='Jimmy Clowes',
+    license='MIT',
+)
